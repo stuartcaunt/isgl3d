0658d11a7859bbbf4b531e5f7dd0f22b46ffdb87
Holger Wiedemann <holger.wiedemann@wirsind.info>
2012-02-23 16:55:25 +0100

    isgl3d-framework project

---
+++ isgl3d-framework/isgl3d.h
@@ -0,0 +1,14 @@
+//
+//  isgl3d.h
+//  isgl3d-framework
+//
+//  Created by Holger Wiedemann on 23.02.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#ifndef isgl3d_framework_isgl3d_h
+#define isgl3d_framework_isgl3d_h
+
+
+
+#endif
---
+++ isgl3d-framework/isgl3dbullet.h
@@ -0,0 +1,14 @@
+//
+//  isgl3dbullet.h
+//  isgl3d-framework
+//
+//  Created by Holger Wiedemann on 23.02.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#ifndef isgl3d_framework_isgl3dbullet_h
+#define isgl3d_framework_isgl3dbullet_h
+
+
+
+#endif
